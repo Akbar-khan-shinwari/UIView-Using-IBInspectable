016320ebecdf4fbf5d091ce668d2eb18150eeea8
Akbar-khan-shinwari <akshinwari1@gmail.com>
2019-02-19 14:07:17 +0500

    My very first commit

---
+++ UIViewUsingIBInspectable.swift
@@ -0,0 +1,64 @@
+//
+//  UIViewUsingIBInspectable.swift
+//  TestingProjectForGitHub
+//
+//  Created by Umer Khalid on 19/02/2019.
+//  Copyright © 2019 Shinwaris. All rights reserved.
+//
+
+import UIKit
+
+// steps
+// 1: download this file and add it in your project.
+// 2: click on the view which you want to modify.
+// 3: go to the attribute inspector and set the values.
+
+
+extension UIView {
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            let color = UIColor.init(cgColor: layer.borderColor!)
+            return color
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+    
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowColor = UIColor.black.cgColor
+            layer.shadowOffset = CGSize(width: 0, height: 2)
+            layer.shadowOpacity = 0.4
+            layer.shadowRadius = shadowRadius
+        }
+    }
+    
+}
